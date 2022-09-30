d765c40be6df943f2e89d100d678e2c60b36b583
maria.montalvo <maria.montalvo@est.fib.upc.edu>
2022-09-30 20:45:40 +0200

    Funcio insert de diccTrie.cc

--- diccTrie.cc
+++ diccTrie.cc
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 
+#define LAST_CHAR '0'
+
 //ESTRUCTURA NODE
 struct Node {
     char* str;
-    Node* leftChild, rigthChild, eqChild; 
+    Node* leftChild, rightChild, eqChild;
 };
 
 //CLASSE ARBRE TERNARI DE CERCA
@@ -13,8 +15,25 @@ class TernarySearchTree {
         Node* root;
 
     public:
+
         TernarySearchTree(){
             this->root = nullptr;
+        }
+
+        insertWord(Node* node, char* word){
+            if(node==nullptr){
+                node->str = word;
+                node->leftChild=nullptr;
+                node->rightChild=nullptr;
+                node->eqChild=nullptr;
+            }
+
+            if(*word < node->str) node->leftChild = insertWord(node->leftChild, word);
+            else if(*word > node->str) node->rightChild = insertWord(node->rightChild, word);
+            else {
+                if(*word!=LAST_CHAR) node->eqChild = insertWord(node->eqChild, ++word);
+            }
+
         }
 
 
