4e04f2261f331b710bf6bf2809ba3d5df5ceb69b
maria.montalvo <maria.montalvo@est.fib.upc.edu>
2022-09-30 20:18:54 +0200

    Creacio de diccTrie.cc

---
+++ diccTrie.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//ESTRUCTURA NODE
+struct Node {
+    char* str;
+    Node* leftChild, rigthChild, eqChild; 
+};
+
+//CLASSE ARBRE TERNARI DE CERCA
+class TernarySearchTree {
+    private:
+        Node* root;
+
+    public:
+        TernarySearchTree(){
+            this->root = nullptr;
+        }
+
+
+};
+
+int main(){
+
+}
