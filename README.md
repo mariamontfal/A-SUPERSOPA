1378d02002808fcb59617e20da0e1a447c23baa5
mariamontfal <maria.montalvo@est.fib.upc.edu>
2022-10-06 17:20:23 +0200

    backtrackingTrie.cc

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # A - SUPERSOPA
-PROJECTE D'ALGORISMIA CURS 2022-2023 Q1
+### PROJECTE D'ALGORISMIA CURS 2022-2023 Q1
 
-[Algorismia FIB](https://www.cs.upc.edu/~mjserna/docencia/grauA/alg-GEI.html)
+### [Algorismia FIB](https://www.cs.upc.edu/~mjserna/docencia/grauA/alg-GEI.html)
+
+-------------------------------------------------------
 
 ## Col·laboradors
+
+-------------------------------------------------------
 
 Ricardo López
 
@@ -14,6 +18,8 @@ Victor Pla
 Sven Wallin 
 
 ## Professors
+
+-------------------------------------------------------
 ### Responsable
 Maria Serna
 
@@ -24,5 +30,31 @@ Conrado Martínez
 
 ## Descripció del projecte
 
+-------------------------------------------------------
+
 El joc del projecte assignat a l’assignatura d’Algorismia del Q1 2022-2023 consisteix en una Super Sopa. La Super Sopa es un tauler de nxn caselles on ha cada casella hi ha una lletra de l’alfabet. Per altra banda, existeix un diccionari on hi ha un conjunt de paraules. Se’ns demana doncs, estudiar l’eficiència i aplicabilitat de diferents algorismes i estructures de dades sobre la cerca de paraules que es podrà dur a terme tant horitzontal, com vertical o diagonalment. L’objectiu final d’aquest joc és trobar en el menor temps possible, totes les paraules de la Super Sopa que siguin part del diccionari. 
 
+
+## Executables
+
+-------------------------------------------------------
+
+
+
+## Codi:
+
+---------------------------------------------------------------------
+
+- diccSortedVector: codi en c++ de l'apartat a) del projecte. Implementació del diccionari amb un vector ordenat.
+
+- diccTrie.cc: codi en c++ de l'apartat b) del projecte. Implementació del diccionari amb un arbre ternari de cerca. 
+
+- diccBloomFilter.cc: codi en c++ de l'apartat c) del projecte. Implementació del diccionari amb un filtre de bloom.
+
+- diccDHashing.cc: codi en c++ de l'apartat c) del projecte. Implementació del diccionari amb doble hasing.
+
+
+
+## Includes:
+
+--------------------------------------------------------------------
---
+++ backtrackingTrie.cc
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+//#include <bits/stdc++.h>
+
+const int N = 10;
+const int ch_MAX = 26;
+
+vector < vector<char> > superSopa;
+vector <string> wordsFound;
+
+
+void generateSopa(){
+    char alpha[ch_MAX] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                          'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                          'o', 'p', 'q', 'r', 's', 't', 'u',
+                          'v', 'w', 'x', 'y', 'z' };
+
+    for(int i=0; i<N; ++i){
+        superSopa.push_back(vector<char>());
+        for(int j=0; j<N; ++j){
+            superSopa[i].push_back(alpha[rand() % ch_MAX]);
+        }
+    }
+}
+
+void printSopa(){
+    for(int i=0; i<N; ++i){
+        for(int j=0; j<N; ++j){
+            cout<<superSopa[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+bool posOk(int i, int j){
+    return i>=0 and i<=N and j>=0 and j<=N;
+}
+
+bool isWord(string mot){
+    //BUSQUEDA ARBRE TERNARI DE CERCA
+    return true;
+}
+
+
+void backtracking(vector < vector <bool> > &visited, string mot, int i, int j){
+    visited[i][j]=1;
+    if (isWord(mot)) wordsFound.push_back(mot);
+
+    if(posOk(i+1,j) and !visited[i+1][j]){
+        mot+=superSopa[i+1][j];
+        backtracking(visited, mot, i+1, j);
+    }
+
+    if(posOk(i,j+1) and !visited[i][j+1]){
+        mot+=superSopa[i][j+1];
+        backtracking(visited, mot, i, j+1);
+    }
+
+    if(posOk(i+1,j+1) and !visited[i+1][j+1]){
+        mot+=superSopa[i+1][j+1];
+        backtracking(visited, mot, i+1, j+1);
+    }
+
+    if(posOk(i-1,j) and !visited[i-1][j]){
+        mot+=superSopa[i-1][j];
+        backtracking(visited, mot, i-1, j);
+    }
+    if(posOk(i,j-1)){
+        mot+=superSopa[i][j-1];
+        backtracking(visited, mot, i, j-1);
+    }
+
+    if(posOk(i-1,j-1)){
+        mot+=superSopa[i-1][j-1];
+        backtracking(visited, mot, i-1, j-1);
+    }
+
+    if(posOk(i-1,j+1)){
+        mot+=superSopa[i-1][j+1];
+        backtracking(visited, mot, i-1, j+1);
+    }
+
+    if(posOk(i+1,j-1)){
+        mot+=superSopa[i+1][j-1];
+        backtracking(visited, mot, i+1, j-1);
+    }
+}
+
+
+int main(){
+    generateSopa();
+    printSopa();
+    vector < vector <bool> > visited(N, vector<bool>(N, 0));
+    string mot = "";
+    backtracking(visited,mot,0,0);
+}
